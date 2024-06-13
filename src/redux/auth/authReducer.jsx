b5a209c7c1bd516b6c09d88d6cc3a84fc1d87735
ankit <ap8424856@gmail.com>
2024-06-13 13:00:03 +0530

    fixed api route path

--- src/redux/auth/authReducer.jsx
+++ src/redux/auth/authReducer.jsx
@@ -2,11 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axiosInstance from '../../utility/Axios';
 
 const signin = createAsyncThunk("auth/signin", async (data, state) => {
-    return await axiosInstance.post("api/auth/signin", data);
+    return await axiosInstance.post("auth/signin", data);
 });
 
 const signup = createAsyncThunk("auth/signup", async (data) => {
-    return await axiosInstance.post("api/user/new", data)
+    return await axiosInstance.post("user/new", data)
 });
 
 
