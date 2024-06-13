08a7f5030b1a8e090c6056a63068e50e71222a93
ankit <ap8424856@gmail.com>
2024-06-13 19:28:58 +0530

    added friend list

--- src/pages/account/FriendsList.jsx
+++ src/pages/account/FriendsList.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const FriendsList = () => {
+  const [username, setusername] = useState("")
+  const friendList = useSelector(state => state.account.friendList)
+
+  const handleRequest = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>FriendsList</div>
+    <div>
+      <h3>friends</h3>
+      <form onSubmit={handleRequest}>
+        <input type="text" placeholder="username" value={username} onChange={e => { setusername(e.target.value) }} />
+        <input type="submit" value="send" />
+      </form>
+      <br />
+      <div>
+        {
+          friendList?.friends?.map(ele => {
+            return <p>{ele}</p>
+          })
+        }
+      </div>
+      <div>
+        {
+          friendList?.incoming?.map(ele => {
+            return <p>{ele}</p>
+          })
+        }
+      </div>
+      <div>
+        {
+          friendList?.outgoing?.map(ele => {
+            return <p>{ele}</p>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
