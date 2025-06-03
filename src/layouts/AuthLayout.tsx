2cf7be91a634c6ef5ad38f2055fa24ff239ff782
Ankit Prajapati <ap8424856@gmail.com>
2025-06-03 21:15:04 +0530

    user login checked in main and auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useAppSelector } from "../state/hooks"
+import { useEffect } from "react"
 
 const AuthLayout = () => {
+  const navigate = useNavigate()
+  const userLoggedIn = useAppSelector(state=> state.userSlice.isLoggedIn)
+
+  useEffect(()=> {
+    if (userLoggedIn) navigate("/chat")
+  }, [userLoggedIn])
+
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <Outlet />
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import MainSidebar from "../comps/organisms/MainSidebar"
+import { useAppSelector } from "../state/hooks"
+import { useEffect } from "react"
 
 
 const MainLayout = () => {
+  const navigate = useNavigate()
+  const userLoggedIn = useAppSelector(state=> state.userSlice.isLoggedIn)
+
+  useEffect(()=> {
+    if (!userLoggedIn) navigate("/auth")
+  }, [userLoggedIn])
+
   return (
     <main className="h-screen overflow-hidden">
       <div className="flex h-full">
